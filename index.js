74eb471856a70ce0767566189d9bb87ece337904
nomad <nomad@odin.com>
2020-06-27 16:19:16 +0300

    Almost working

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-require('dotenv').config();
+const config = require('dotenv').config();
+const fs = require('fs');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+
+
+const prefix = process.env.PREFIX;
 const TOKEN = process.env.TOKEN;
+
+const getBoyarInfo = function(){
+  var bi = new Discord.MessageEmbed();
+  bi.setColor('#0099ff')
+  .setTitle('Боярская инфа')
+  .setDescription('Боярин сей многими свойствами обладает')
+  .addFields(
+		{ name: 'Характер', value: 'Нордический' },
+    { name: 'Уменья', value: 'Космические'})
+  .setTimestamp()
+  .setFooter('Многие лета!');
+  return bi;
+};
+
+function getFileName(id){
+  return './savedInfo/usr' + id + '.json';
+}
+
+function saveInfo(fname, dat, msg){
+  fs.writeFile(fname, dat , function (err,data) {
+    if(err){
+        msg.channel.send('File writing error');
+    }else{
+        msg.channel.send('Success!');
+    }
+  });
+}
+
+function loadInfo(fname, msg, cb){
+  var boyarInfo = new Discord.MessageEmbed(),
+      is_new = true;
+  if(fs.existsSync(fname)){
+    msg.channel.send(`О нём нам ведомо!`);
+    let data = JSON.parse(fs.readFileSync(fname));
+    console.debug('data', data);
+    boyarInfo = new Discord.MessageEmbed(data);
+    is_new = false;
+    console.debug('boyarInfo', boyarInfo);
+  }else{
+    msg.channel.send(`О нём не знаем!`);
+  }
+  return { data: boyarInfo, is_new: is_new }
+}
 
 bot.login(TOKEN);
 
@@ -10,10 +57,70 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong Мать твою!');
-    msg.channel.send('pong');
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+  console.debug('go');
+  if (msg.mentions.users.size != 1) return;
+  const taggedUser = msg.mentions.users.first();
+  const fn = getFileName(taggedUser.id);
+  const args = msg.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
+  if (command === 'set') {
+    msg.reply('Боярин ли?');
+    if (msg.member.roles.cache.some(role => role.name === 'bot-master')){
+      msg.channel.send('Боярин!!');
+      console.debug('args', args);
+      let info = loadInfo(fn, msg).data;
+      if(args[1] === 'descr'){
+        let idx = msg.content.indexOf('descr')+'descr'.length;
+        let descrTxt = msg.content.substring(idx);
+        console.debug('rest', descrTxt);
+        msg.channel.send('Описаньице!');
+        info.setDescription(descrTxt);
+      }else{
+        msg.channel.send('Незнаемо!');
+      }
+
+      msg.channel.send(`Запоминаем о: ${taggedUser.username}`);
+      /*
+      const tmp = new Discord.MessageEmbed(data.data).setColor('#0099ff')
+      .setTitle('Боярская инфа')
+      .setDescription('Боярин сей многими свойствами обладает')
+      .addFields({name: 'Характер', value: 'Стоический'},{name: 'Возраст', value: 'Преклонный'}); */
+      const dt = JSON.stringify(info);
+      saveInfo(fn, dt, msg);
+    }else{
+      msg.channel.send('Не боярин!');
+    }
+  } else if (command === 'get') {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`Вопрошаешь о: ${taggedUser.username}`);
+      try{
+        let data = loadInfo(fn, msg);
+        if(!data.is_new){
+          msg.channel.send(data.data)
+        }
+      } catch(err) {
+        console.error(err);
+      }
+
+    } else {
+      msg.reply('Не знаю о ком сие!');
+    }
+  } else if (command === 'del') {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`Стираем: ${taggedUser.username}`);
+      fs.unlink(getFileName(taggedUser.id), (err) => {
+        if (err) {
+          console.error(err)
+        }
+        msg.reply('Забыли');
+      });
+    } else {
+      msg.reply('Не знаем такого!');
+    }
   } else if (msg.content.startsWith('!kick')) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
